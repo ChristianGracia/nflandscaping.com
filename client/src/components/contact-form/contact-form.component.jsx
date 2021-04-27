d2e5c1c741477891d141e0e0cb96aaca2ac692ca
Christian Gracia <christiangracia.student@careerdevs.com>
2021-04-26 22:49:50 -0400

    changed email api request to target my personal api for now

--- client/src/components/contact-form/contact-form.component.jsx
+++ client/src/components/contact-form/contact-form.component.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { useState } from "react";
-import { Form, Col, Button } from "react-bootstrap";
-import LightSpeed from "react-reveal/LightSpeed";
-import "./contact-form.styles.scss";
+import React from 'react';
+import { useState } from 'react';
+import { Form, Col, Button } from 'react-bootstrap';
+import LightSpeed from 'react-reveal/LightSpeed';
+import './contact-form.styles.scss';
 
 const ContactForm = () => {
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -18,64 +18,64 @@ const ContactForm = () => {
     setValidated(true);
   };
   return (
-    <div className="contact-container">
+    <div className='contact-container'>
       <Form
         noValidate
         validated={validated}
-        action="https://sendpoint.io/id/nflcontactform"
-        method="POST"
+        action='https://christiangracia-api-staging.herokuapp.com/email/send-email-nfl'
+        method='POST'
         onSubmit={handleSubmit}
         style={{}}
-        className="ml-auto mr-auto"
+        className='ml-auto mr-auto'
       >
         <div style={{ padding: 30 }}></div>
         <Form.Group
-          className="ml-auto mr-auto"
+          className='ml-auto mr-auto'
           as={Col}
-          md="4"
-          controlId="validationCustom01"
+          md='4'
+          controlId='validationCustom01'
         >
-          {" "}
+          {' '}
           <LightSpeed left>
-            <Form.Label className="input-label">Name</Form.Label>
+            <Form.Label className='input-label'>Name</Form.Label>
           </LightSpeed>
-          <Form.Control required type="text" placeholder="Name" name="name" />
+          <Form.Control required type='text' placeholder='Name' name='name' />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group
-          className="ml-auto mr-auto"
+          className='ml-auto mr-auto'
           as={Col}
-          md="4"
-          controlId="validationCustom02"
+          md='4'
+          controlId='validationCustom02'
         >
           <LightSpeed right>
-            <Form.Label className="input-label">Phone number</Form.Label>
+            <Form.Label className='input-label'>Phone number</Form.Label>
           </LightSpeed>
           <Form.Control
             required
-            type="tel"
-            placeholder="XXX-XXX-XXXX"
-            name="phone"
+            type='tel'
+            placeholder='XXX-XXX-XXXX'
+            name='phone'
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group
-          className="ml-auto mr-auto message-input"
-          controlId="exampleForm.ControlTextarea1"
+          className='ml-auto mr-auto message-input'
+          controlId='exampleForm.ControlTextarea1'
         >
           <LightSpeed left>
-            <Form.Label className="input-label">Message</Form.Label>
+            <Form.Label className='input-label'>Message</Form.Label>
           </LightSpeed>
           <Form.Control
             required
-            as="textarea"
-            rows="3"
-            name="message"
-            placeholder="I would like more information regarding..."
+            as='textarea'
+            rows='3'
+            name='message'
+            placeholder='I would like more information regarding...'
           />
         </Form.Group>
 
-        <Button type="submit" className="btn-danger">
+        <Button type='submit' className='btn-danger'>
           Submit form
         </Button>
       </Form>
